8b45a6f387899ad010fe29faa7978eb1632a05ab
mixmix <whimful@gmail.com>
2018-11-08 23:41:41 +1300

    search box!

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const html = require('yo-yo')
 const { fabric } = require('fabric')
 const Connection = require('ssb-client')
+const map = require('lodash.map')
+const suggestify = require('suggest-box')
+const onload = require('on-load')
 
 const config = require('./config')
 const exportData = require('./lib/export-data')
 const addImage = require('./lib/add-image')
 const importData = require('./lib/import-data')
+const css = require('./styles')
 
 const blobId = '&QT5YAIrMuMeCYYT1ncJtDMJ09Br9yl3M6+Xb5PERWgU=.sha256'
+
+document.head.appendChild(html`
+  <style>${css}</style>
+`)
 
 Connection((err, server) => {
   if (err) throw err
 
-  document.body.appendChild(BlobParty())
+  document.body.appendChild(BlobParty(server))
   document.body.style.background = 'black'
 })
 
-function BlobParty () {
+function BlobParty (server) {
   var canvas
+
   const blobParty = html`
     <div className="BlobParty">
       <canvas id="c" height="${config.height}" width="${config.width}"></canvas>
@@ -26,14 +35,14 @@ function BlobParty () {
       <button onclick=${() => importData({ canvas, blobUrl })}>Load Save</button>
     </div>
   `
-
-  setCanvas()
-  return blobParty
-  function setCanvas () {
-    if (!blobParty.isConnected) return setTimeout(setCanvas, 200)
+  onload(blobParty, () => {
     canvas = new fabric.Canvas('c')
     canvas.fabric = fabric
-  }
+
+    blobParty.appendChild(Search(server, canvas))
+  })
+
+  return blobParty
 }
 
 function blobUrl (blobId) {
@@ -49,21 +58,54 @@ function blobUrl (blobId) {
 // - import state
 // - diff states
 
-// function toSuggestion (value, key) {
-//   const topPick = value[0]
-//   // TODO get a better topPick (filter by user, frequency etc?)
-//   //
-//   // return {
-//   //   image: api.blob.sync.url(key),
-//   //   title: topPick.name,
-//   //   // subtitle: topPick.name,
-//   //   value: '![' + topPick.name + '](' + key + ')'
-//   // }
-//   //
-//   return {
-//     // image:
-//     title: h('img.meme', { src: api.blob.sync.url(key) }),
-//     subtitle: topPick.name,
-//     value: '![' + topPick.name + '](' + key + ')'
-//   }
-/* } */
+function Search (server, canvas) {
+  const input = html`
+    <input ></input>
+  `
+  onload(input, () => {
+    suggestify(input, suggestBlob(server), { cls: 'PatchSuggest' })
+    input.addEventListener('suggestselect', ev => {
+      addImage({ canvas, blobUrl })(ev.detail.value)
+      input.value = ''
+    })
+  })
+
+  return input
+}
+
+function suggestBlob (server) {
+  return function (word, cb) {
+    if (word.length < 3) return
+    // TODO suggest a random 10?
+
+    word = word.toLowerCase()
+
+    server.meme.search(word, (err, data) => {
+      if (err) return console.error(err)
+
+      cb(null, map(data, toSuggestion))
+    })
+  }
+}
+
+function toSuggestion (value, key) {
+  const topPick = value[0]
+  // TODO get a better topPick (filter by user, frequency etc?)
+  //
+  // return {
+  //   image: api.blob.sync.url(key),
+  //   title: topPick.name,
+  //   // subtitle: topPick.name,
+  //   value: '![' + topPick.name + '](' + key + ')'
+  // }
+  //
+  return {
+    // image:
+    title: html`
+      <img class='meme' src='${blobUrl(key)}' />
+    `,
+    subtitle: topPick.name,
+    // value: '![' + topPick.name + '](' + key + ')'
+    value: key
+  }
+}
---
+++ styles.js
@@ -0,0 +1,123 @@
+const compile = require('micro-css')
+
+const mcss = `
+PatchSuggest {
+  z-index: 999
+  overflow-y: auto
+  background-color: #fff
+
+  width: max-content
+  min-width: 20rem
+  max-width: 35rem
+
+  max-height: 70vh
+
+  padding: .2rem .5rem
+  border: 1px gainsboro solid
+  margin-top: .35rem
+
+  ul {
+    list-style-type: none
+    padding: 0
+
+    li {
+      display: flex
+      align-items: center
+
+      padding-right: .2rem
+      margin-bottom: .2rem
+
+      img {
+        height: 36px
+        width: 36px
+        min-width: 36px
+        padding: .2rem
+      }
+
+      strong {
+        font-weight: 300
+        min-width: 7rem
+        margin-left: .5rem
+        margin-right: .5rem
+
+        // *** this is a hack for ssb-memes!
+        display: flex
+        align-items: center
+
+        img.meme {
+          max-height: 100px
+          width: 100px
+          padding: .2rem
+        }
+        // ***
+
+        span.subtle {
+          color: #aaa
+        }
+      }
+
+      small {
+        flex-grow: 1
+
+        margin-left: .5rem
+        padding-right: .2rem
+        font-size: 1rem
+
+        display: flex
+        justify-content: flex-end
+
+        // profile only
+        div.aliases {
+          flex-grow: 1
+
+          font-size: .8rem
+          color: #666
+          margin-right: .5rem
+
+          display: flex
+          flex-wrap: wrap
+
+          div.alias {
+            margin-right: .4rem
+            -bold {
+              font-weight: 600
+            }
+          }
+        }
+
+        // profile only
+        div.key {
+          align-self: flex-end
+
+          margin: auto 0
+
+          font-family: monospace
+          font-size: .8rem
+          min-width: 5rem
+        }
+
+        // emoji only
+        div.emoji {
+          flex-grow: 1
+        }
+
+      }
+    }
+
+    li.selected {
+      color: #fff
+      background: #0caaf9
+
+      img {}
+      strong {}
+      small {
+        div.aliases {
+          color: #eee
+        }
+      }
+    }
+  }
+}   
+`
+
+module.exports = compile(mcss)
